91259d9dc1e80520904f561ea795dd143b1bd4ea
TTTT0803 <trinhthanhthao2k5@gmail.com>
2024-06-21 23:25:05 +0700

    backup

---
+++ BWD_VKUMentor/Javascript/api/login.js
@@ -0,0 +1,30 @@
+const btnLogin = document.getElementById("btnLogin");
+
+btnLogin.addEventListener("click", () => {
+  const txtUsername = document.getElementById("txtUsername");
+  const txtPassword = document.getElementById("txtPassword");
+
+  if (txtUsername.value.trim() === "" || txtPassword.value.trim() === "") {
+    alert("Bạn chưa nhập tài khoản hoặc mật khẩu")
+    return
+  }
+
+  axios
+    .post("http://127.0.0.1:8000/api/NguoiDung/login", {
+      "TenDangNhap": txtUsername.value,
+      "MatKhau": txtPassword.value,
+    })
+    .then((response) => {
+      if (response && response.data) {
+        
+        if (response.data) {
+            localStorage.setItem("isLogin", true)
+            window.location.href = "./../../index.html"
+        }
+
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
